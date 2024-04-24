035d7175e62c698d0acd5e28fb42942d268190b8
Ilya <129628826+Hyakkkimaru@users.noreply.github.com>
2024-04-24 19:29:34 +0300

    Create file Merge_Sorted_Array.cpp

---
+++ algorithms/C++/MergeSortedArray/Merge_Sorted_Array.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution {
+public:
+    void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+        int index1 = m - 1;
+        int index2 = n - 1;
+        int mergedIndex = m + n - 1;
+
+        while (index1 >= 0 && index2 >= 0) {
+            if (nums1[index1] > nums2[index2]) {
+                nums1[mergedIndex--] = nums1[index1--];
+            } else {
+                nums1[mergedIndex--] = nums2[index2--];
+            }
+        }
+
+        while (index2 >= 0) {
+            nums1[mergedIndex--] = nums2[index2--];
+        }
+    }
+};
